af23ccf2054352bffa0d688b966f2eba9267fd10
kamrul-i <komrul45@gmail.com>
2024-05-09 19:19:00 +0200

    6th, Show decimal number result.

--- src/user_input/User_input_Products.java
+++ src/user_input/User_input_Products.java
@@ -8,13 +8,13 @@ public class User_input_Products {
 		
 		//কিস্তিতে কোন কিছু কেনার মতো ।। দোকানি দাম নির্দিষ্ট করে দিল, তারপর আমরা কিস্তি দিলাম।।
 		try (Scanner input = new Scanner(System.in)) {
-			int phonePrice = 1800;
-			int num, result;
+			double phonePrice = 1800;
+			int num;
 			
 			System.out.print("Number of installments : ");
 			num = input.nextInt();
 			
-			result = phonePrice / num;
+			double result = (double) phonePrice / num;
 			System.out.print("Monthly installments Amount : " +result+ " euros.");
 		}
 		
