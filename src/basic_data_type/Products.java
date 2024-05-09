3bdfa0bcb084bfd15309a948ecf44b6f20a07352
kamrul-i <komrul45@gmail.com>
2024-05-09 18:08:58 +0200

    1st commit, basic data-type

---
+++ src/basic_data_type/Products.java
@@ -0,0 +1,19 @@
+package basic_data_type;
+
+public class Products {
+	
+	public static void main(String[] args) {
+		
+		
+		//========================================
+		System.out.println("id:          101");
+		System.out.println("title:       iphone15");
+		System.out.println("price:       1895 euros");
+		System.out.println("description: perfect products");
+		System.out.println("category:    phone");
+		//========================================
+		
+
+	}
+
+}
