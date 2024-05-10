8ed583e69b4eec3e4b997a2f3ff75593c5844e7c
kamrul-i <komrul45@gmail.com>
2024-05-10 15:22:08 +0200

    Data type "Variable".

---
+++ src/basic_data_type/Data_type.java
@@ -0,0 +1,31 @@
+package basic_data_type;
+
+public class Data_type {
+	public static void main(String[] args) {	
+		
+		//এখানে variable ধরে একটা একটা করে সাথে সাখে print করে দেখানো হয়েছে।।
+		boolean b;
+		b = true;
+		System.out.println("b = "+b);
+       
+		char c;
+		c = 'a';
+		System.out.println("c = "+c);
+       
+		short s;
+		s = 32111;
+		System.out.println("s = "+s);
+       
+ 
+		//এখানে একসাথে সকল variable ধরে আবার নিচে একসাথে print করে দেখানো হয়েছে।।
+		int i = 125;
+		float f = 787.8785f;  //float এর মান যা ধরবেন, তার সাথে f না দিলে হবে না ।।
+		double d = 787.73;
+             
+		System.out.println("i = "+i);
+		System.out.println("f = "+f);
+		System.out.println("d = "+d);
+      
+	}
+
+}
