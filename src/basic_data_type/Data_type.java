d8be4eb3d1059507a5446d4234ebbbd7d7baabb3
kamrul-i <komrul45@gmail.com>
2024-05-10 15:37:08 +0200

    using "printf"

--- src/basic_data_type/Data_type.java
+++ src/basic_data_type/Data_type.java
@@ -3,32 +3,20 @@ package basic_data_type;
 public class Data_type {
 	public static void main(String[] args) {	
 		
-		//এখানে variable ধরে একটা একটা করে সাথে সাখে print করে দেখানো হয়েছে।।
-		boolean b;
-		b = true;
-		System.out.println("b = "+b);
-       
-		char c;
-		c = 'a';
-		System.out.println("c = "+c);
-       
-		short s;
-		s = 32111;
-		System.out.println("s = "+s);
-       
- 
-		//এখানে একসাথে সকল variable ধরে আবার নিচে একসাথে print করে দেখানো হয়েছে।।
-		int i = 125;
-		float f = 787.878545584f;  //float এর মান যা ধরবেন, তার সাথে f না দিলে হবে না ।।
-		double d = 787.73498654;   
-             
-		System.out.println("i = "+i);
-		System.out.printf("f = %.2f", f);
-		System.out.printf("\n");
-		System.out.printf("d = %.1f", d);
-		
-		//দশমিকের পরে result বেশি থাকলে, দশমিকের পরে কত ঘর show করবে তা select করে দেওয়া হয়েছে, 
-		//তবে তার জন্য "printf" use করতে হবে।।
+        boolean b = true;
+        char c = 'a';
+        short s = 4578;
+        int i = 125;
+        float f = 787.87401f;   //float এর মান যা ধরবেন, তার সাথে f না দিলে হবে না ।।
+        double d = 787.73120;
+                                      
+        
+        System.out.printf("boolean b = %b\n", b);
+        System.out.printf("character c = %c\n", c);
+        System.out.printf("short s = %d\n", s);
+        System.out.printf("intager i = %d\n", i);
+        System.out.printf("floar f = %.2f\n", f);
+        System.out.printf("double d = %.2f\n", d);
       
 	}
 
